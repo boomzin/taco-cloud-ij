ad513c97c42a423f4c1e192b1f84f6cf0dd31660
boomzin windows work <boomzin@gmail.com>
2021-06-16 19:31:06 +0300

    Initial commit

---
+++ src/main/java/tacos/tacocloudij/TacoCloudIjApplication.java
@@ -0,0 +1,13 @@
+package tacos.tacocloudij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudIjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudIjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tacos/tacocloudij/TacoCloudIjApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos.tacocloudij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudIjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
